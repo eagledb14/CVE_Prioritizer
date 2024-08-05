63391c080e4e635b5ec68bcc5a7daa285dddb982
eagledb14 <eagledb14@gmail.com>
2024-08-05 15:11:12 -0400

    osint.sh now no longer spams if there are no cves

--- osint.sh
+++ osint.sh
@@ -11,17 +11,25 @@
 #
 ######
 
-host="google.com"
+host=""
 touch ips.txt
 
 shodan search --fields ip_str --limit 100 $host >> ips.txt || true
 
 readarray -t ips < ips.txt
 
-for ip in "${ips[@]}"; do
-    echo $ip | xargs -n 1 shodan host | grep Vulnerabilities|  grep -oE 'CVE-[0-9]{4}-[0-9]{4,}'> cves.txt
+touch cves
 
-    python3 cve_prioritizer.py -vf cves.txt -o "${ip}_cve_prioritizer_output.csv"
+for ip in "${ips[@]}"; do
+    echo $ip | xargs -n 1 shodan host | grep Vulnerabilities|  grep -oE 'CVE-[0-9]{4}-[0-9]{4,}'> cves
+
+    cat cves
+
+    if [ ! -s "$cves"]; then
+        python3 cve_prioritizer.py -vf cves.txt -o "${ip}_cve_prioritizer_output.csv"
+    fi
     # awk -F ',' 'NR==1{$0=toupper($0)} {OFS = ","; gsub("CVSS_", "", $5); gsub("CVSS_", "", $6); print $1,$2,$3,$4,$5,$6,$7,$9}' "${ip}_cve_prioritizer_output.csv" > "${ip}_cve_prioritizer_output_modified.csv"
     # rm "${ip}_cve_prioritizer_output.csv"
 done
+
+rm cves
